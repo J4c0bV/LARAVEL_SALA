9f341ad46ff6768990776405fee46f3fff9263f8
João Vitor Vasconcelos Jacob <135768611+J4c0bV@users.noreply.github.com>
2024-03-16 10:26:59 -0300

    Aula 16/03 alteração no controller

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <h2>Bem vindo ao index</h2>
 
-@foreach ($varContatos as $varContato)
-    <p>Nome: {{$varContato['nome']}}</p>
-    <p>Numero: {{$varContato['cel']}}</p>
+@foreach ($varTemas as $varTema)
+    <p>Tema: {{$varTema['tema']}}</p>
+    <p>Titulo: {{$varTema['titulo']}}</p>
 @endforeach
